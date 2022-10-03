dde366f752d30626d3af3cb738e38d78db809f39
Jose Luis <unixluisx@protonmail.ch>
2022-10-02 23:29:34 -0500

    operators

--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,41 @@ public class Main {
 
 
         String myString = "This is a string";
-        System.out.println("myString = " + myString);
+        //System.out.println("myString = " + myString);
         myString = myString + ", and this more";
-        System.out.println("myString = " + myString);
+        //System.out.println("myString = " + myString);
         myString = myString + "\u00A9 2022";
-        System.out.println("myString = " + myString);
+        //System.out.println("myString = " + myString);
 
         String numberString = "250.55";
         numberString = numberString + "49.5";
-        System.out.println("numberString = " + numberString);
+        //System.out.println("numberString = " + numberString);
 
         String lastString = "10";
         int myInt = 50;
         lastString = lastString + myInt;
-        System.out.println("lastString = " + lastString);
+        //System.out.println("lastString = " + lastString);
 
         double doubleNumber = 120.47d;
         lastString = lastString + doubleNumber;
-        System.out.println("lastString = " + lastString);
+        //System.out.println("lastString = " + lastString);
+
+        //operators
+
+        int result = 1 + 2;
+        System.out.println("1 + 2  = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);
     }
 }

b62279b5f2d96d22c07ad520164738124b3056fd
Jose Luis <unixluisx@protonmail.ch>
2022-10-02 22:59:48 -0500

    The Char and boolean

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char unicode = '\u00C0';
+        System.out.println("unicode = " + unicode);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean IsCustomerOverTwentyOne = true;
+
+    }
+}

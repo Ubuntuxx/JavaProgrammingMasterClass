1197fab5ae7060c9df5497c368cc0f8941314b3e
Jose Luis <unixluisx@protonmail.ch>
2022-10-02 23:16:48 -0500

    primitive data types recap and Strings

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,32 @@ public class Main {
     public static void main(String[] args) {
         char myChar = 'D';
         char unicode = '\u00C0';
-        System.out.println("unicode = " + unicode);
+        //System.out.println("unicode = " + unicode);
 
         boolean myTrueBooleanValue = true;
         boolean myFalseBooleanValue = false;
 
         boolean IsCustomerOverTwentyOne = true;
 
+
+        String myString = "This is a string";
+        System.out.println("myString = " + myString);
+        myString = myString + ", and this more";
+        System.out.println("myString = " + myString);
+        myString = myString + "\u00A9 2022";
+        System.out.println("myString = " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.5";
+        System.out.println("numberString = " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString = " + lastString);
+
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString = " + lastString);
     }
 }

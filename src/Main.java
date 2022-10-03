fb4d35cbe9d81eb8299a048dec03dd51a14b51b4
Jose Luis <unixluisx@protonmail.ch>
2022-10-03 01:10:52 -0500

    If-Then statement

--- src/Main.java
+++ src/Main.java
@@ -33,19 +33,26 @@ public class Main {
         //operators
 
         int result = 1 + 2;
-        System.out.println("1 + 2  = " + result);
+        //System.out.println("1 + 2  = " + result);
         int previousResult = result;
-        System.out.println("previousResult = " + previousResult);
+        //System.out.println("previousResult = " + previousResult);
         result = result - 1;
-        System.out.println("3 - 1 = " + result);
+        //System.out.println("3 - 1 = " + result);
 
         result = result * 10;
-        System.out.println("2 * 10 = " + result);
+        //System.out.println("2 * 10 = " + result);
 
         result = result / 5;
-        System.out.println("20 / 5 = " + result);
+        //System.out.println("20 / 5 = " + result);
 
         result = result % 3;
-        System.out.println("4 % 3 = " + result);
+        //System.out.println("4 % 3 = " + result);
+
+        // if-Then statement
+        boolean isAlien = false;
+        if (isAlien) {
+            System.out.println(" IS not a alien");
+            System.out.println("Es un alien");
+        }
     }
 }

e9b7b8456a7e91653855b816863b5214e48352d5
Jose Luis <unixluisx@protonmail.ch>
2022-10-05 14:51:17 -0500

    methods

--- src/Code.java
+++ src/Code.java
@@ -1,25 +1,25 @@
 public class Code {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 5000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-//        if (score <= 500 && score > 1000) {
-//            System.out.println("Your score was less than 5000");
-//        } else if (score < 1000) {
-//            System.out.println("your score was less than 1000");
-//        } else {
-//            System.out.println("Got here");
-//        }
+        calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10000, 8, 100);
+
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("your final score was" + finalScore);
+            finalScore += 2000;
+            System.out.println("your final score was = " + finalScore);
+            return finalScore;
         }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        return -1;
     }
 }

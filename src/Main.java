229adec0ab4dace73a05941e3230918c415a9095
Jose Luis <unixluisx@protonmail.ch>
2022-10-18 22:18:59 -0500

    Overloading

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+        int newScore = calculateScore("Luis", 499);
+        System.out.println("New score is " + newScore);
+    }
 
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unamed player scored " + score + " points");
+        return score * 1000;
     }
 }

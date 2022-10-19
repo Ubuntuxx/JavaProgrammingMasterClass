969cd98f3247bba58eb14de4c043fcc63270733b
Jose Luis <unixluisx@protonmail.ch>
2022-10-18 22:41:18 -0500

    Overloading excercise

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int newScore = calculateScore("Luis", 499);
-        System.out.println("New score is " + newScore);
+        calcFeetAndInchesToCentimeters(6, 0);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -12,5 +11,25 @@ public class Main {
     public static int calculateScore(int score) {
         System.out.println("Unamed player scored " + score + " points");
         return score * 1000;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || ((inches < 0) && (inches > 12))) {
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet " + inches + " inches " + centimeters + " centimeters");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            return -1;
+        }
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inchcs is equal to " + feet + " feet and " + remainingInches);
+        return calcFeetAndInchesToCentimeters(feet, inches);
     }
 }
